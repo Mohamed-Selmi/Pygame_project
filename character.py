398c81585893b3b7d801b83492102d3217ca51f6
Mohamed-Selmi <mohamedamineselmi@outlook.com>
2025-03-28 15:26:52 +0100

    Loaded a spritesheet and added idle and moving animation

--- character.py
+++ character.py
@@ -1,35 +1,69 @@
 import pygame
 class Character():
-    def __init__(self,x,y):
+    def __init__(self,x,y,flip,data,sprite_sheet,animation_steps):
+        self.size=data[0]
+        self.image_scale=data[1]
+        self.offset=data[2]
+        self.animation_list=self.load_images(sprite_sheet,animation_steps)
+        self.action_list={"idle":0,"moving":1,"jumping":2,"attack1":3,"attack2":4,"hit":5,"death":6}
+        self.action=0
+        self.frame_index=0
+        self.image=self.animation_list[self.action][self.frame_index]
         self.rect=pygame.Rect((x,y,80,180))
         self.velocity_y=0
         self.jumping=False
+        self.running=False
         self.attack_type=0
-    def draw(self,surface):
-        pygame.draw.rect(surface,(0,255,0),self.rect)
+        self.attacking=False
+        self.health=100
+        self.flip=flip
+        self.update_time=pygame.time.get_ticks()
 
-    def move(self,screen_width,screen_height,surface):
+
+    def load_images(self,sprite_sheet,animation_steps):
+        animation_list=[]
+        for y,animation in enumerate(animation_steps):
+            temp_img_list=[]
+            for x in range(animation):
+                temp_img=sprite_sheet.subsurface(x*self.size,y*self.size,self.size,self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img,(self.size*self.image_scale,self.size*self.image_scale)))
+            animation_list.append(temp_img_list)
+        
+        return animation_list    
+    def draw(self,surface):
+        rotating_img=pygame.transform.flip(self.image,self.flip,False)
+        pygame.draw.rect(surface,(0,255,0),self.rect)
+        surface.blit(rotating_img,(self.rect.x-(self.offset[0]*self.image_scale),self.rect.y-(self.offset[1]*self.image_scale)))
+
+    def move(self,screen_width,screen_height,surface,target):
         CHARACTER_SPEED=10
         CHARACTER_GRAVITY=2
         move_x=0
         move_y=0
-         
+        self.running=False
         key=pygame.key.get_pressed()
-        if key[pygame.K_a]:
-            move_x= -CHARACTER_SPEED
-        if key[pygame.K_d]:
-            move_x= +CHARACTER_SPEED
 
-        if key[pygame.K_w] and self.jumping == False:
-            self.velocity_y=-30
-            self.jumping=True
-        self.velocity_y+=CHARACTER_GRAVITY
-        if key[pygame.K_r] or key[pygame.K_t]:
-            self.attack(surface)
-            if key[pygame.K_r]:
-                self.attack_type=1
-            if key[pygame.K_r]:
-                self.attack_type=2           
+        if self.attacking==False:
+            if key[pygame.K_a]:
+                self.running=True
+                #self.action=self.action_list["moving"]
+                move_x= -CHARACTER_SPEED
+            if key[pygame.K_d]:
+                self.running=True
+                #self.action=self.action_list["moving"] 
+                move_x= +CHARACTER_SPEED
+
+            if key[pygame.K_w] and self.jumping == False:
+                self.velocity_y=-30
+                self.jumping=True
+            self.velocity_y+=CHARACTER_GRAVITY
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface,target)
+                if key[pygame.K_r]:
+                    self.attack_type=1
+                if key[pygame.K_r]:
+                    self.attack_type=2 
+                        
         move_y+=self.velocity_y
         if self.rect.left+move_x<0:
             move_x=-self.rect.left
@@ -40,11 +74,27 @@ class Character():
             self.velocity_y=0
             self.jumping=False
             move_y=screen_height-220-self.rect.bottom           
-
-
+        if target.rect.centerx>self.rect.centerx:
+            self.flip=False
+        else:
+            self.flip=True
         self.rect.x+=move_x
         self.rect.y+=move_y
-    def attack(self,surface):
-        attacking_rectangle=pygame.Rect(self.rect.centerx,self.rect.y,2*self.rect.width,self.rect.height)
-        if attacking_rectangle.colliderect()
+
+
+    def update_character(self):
+        animation_cooldown=50 #milliseconds
+        self.image=self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks()-self.update_time>animation_cooldown:
+            self.frame_index+=1
+            self.update_time=pygame.time.get_ticks()
+            if self.frame_index>=len(self.animation_list[self.action]):
+                self.frame_index=0
+
+    def attack(self,surface,target):
+        self.attacking=True
+        attacking_rectangle=pygame.Rect(self.rect.centerx-(2*self.rect.width*self.flip),self.rect.y,2*self.rect.width,self.rect.height)
+        if attacking_rectangle.colliderect(target.rect):
+            target.health-=10
         pygame.draw.rect(surface,(255,0,0),attacking_rectangle)
+
--- main.py
+++ main.py
@@ -11,32 +11,55 @@ pygame.display.set_caption("Pygame Project")
 
 clock=pygame.time.Clock()
 FPS=60
+YELLOW=(255,255,0)
+RED=(255,0,0)
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+
+WARRIOR_SIZE=162
+WARRIOR_SCALE=4
+WARRIOR_OFFSET=[72,56]
+WARRIOR_DATA=[WARRIOR_SIZE,WARRIOR_SCALE,WARRIOR_OFFSET]
+
 
 background_image=pygame.image.load("Pygame_project/assets/images/background.jpg").convert_alpha()
+warrior_sheet=pygame.image.load("Pygame_project/assets/warrior/Sprites/warrior1.png").convert_alpha()
+warrior_animation_steps=[10,8,1,7,7,3,7]
+king_animation_steps=[8,8,2,2,4,4,4,6]
 
 def load_image():
     scaled_image=pygame.transform.scale(background_image,(SCREEN_WIDTH,SCREEN_HEIGHT))
     screen.blit(scaled_image,(0,0))
 
+def draw_health_bar(health,x,y):
+    bar_ratio=health/100
+    pygame.draw.rect(screen,BLACK,(x-5,y-5,410,40))
+    pygame.draw.rect(screen,RED,(x,y,400,30))
+    pygame.draw.rect(screen,YELLOW,(x,y,400*bar_ratio,30))
+    
 
 
 
-character1=Character(200,510)
+character1=Character(200,510,False,WARRIOR_DATA,warrior_sheet,warrior_animation_steps)
 
-character2=Character(1200,510)
+character2=Character(1200,510,True,WARRIOR_DATA,warrior_sheet,warrior_animation_steps)
 
 game_running=True
 
 while game_running:
     clock.tick(FPS)
     load_image()
-    character1.move(SCREEN_WIDTH,SCREEN_HEIGHT,screen)
+    draw_health_bar(character1.health,20,20)
+    draw_health_bar(character2.health,1020,20)
+    character1.move(SCREEN_WIDTH,SCREEN_HEIGHT,screen,character2)
+    character1.update_character()
+    character2.update_character()
    #  character2.move()
     character1.draw(screen)
     character2.draw(screen)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            game_running=False
+            game_running=True
 
     pygame.display.update()
 pygame.quit()
